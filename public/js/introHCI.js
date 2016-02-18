001c0f586cc16e74b9445e98873d2c8147950601
Eugene Temlock <etemlock@ucsd.edu>
2016-02-18 00:20:37 -0800

    I'm such an idiot

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	
 }
+
+$(".likeCtr").click(function(e){
+	e.preventDefault();
+	ga('send','event','Like','click');
+})
